bdcb00801d7034569911d36b1a87f73be358bc70
Tadeusz Sośnierz <tadzikes@gmail.com>
2010-07-21 19:10:44 +0000

    Documentation added

--- lib/Config/INI.pm
+++ lib/Config/INI.pm
@@ -53,3 +53,39 @@ our sub parse_file (Str $file) {
 	}
 	return %result
 }
+
+=begin pod
+
+=head1 NAME
+Config::INI - parse standard configuration files (.ini files)
+
+=head1 SYNOPSIS
+	use Config::INI;
+	my %hash = Config::INI::parse_file('config.ini');
+	#or
+	%hash = Config::INI::parse($file_contents);
+	say %hash<root_property_key>;
+	say %hash<section><in_section_key>;
+
+=head1 DESCRIPTION
+
+This module provides 2 functions: parse() and parse_file(), both taking
+one C<Str> argument, where parse_file is just parse(slurp $file).
+Both return a hash which keys are either toplevel keys or a section
+names. For example, the following config file:
+
+	foo=bar
+	[section]
+	another=thing
+
+would result in the following hash:
+
+	{ foo => "bar", section => { another => "thing" } }
+
+=head1 CAVEATS
+
+Parser will fail if a file contains either empty sections, or keys
+without specified value (as in 'foo='). Whether this is a bug
+or a feature, time will tell.
+
+=end pod
